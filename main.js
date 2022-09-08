af9f0d373abac533384d384644f82854910fb776
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-07 20:40:35 -0400

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let name = ["jon", "bob", "tom", "kenneth"]
+// for (let i = 0; i < name.length; i++){
+//     console.log(name[i].toUpperCase())
+// }
+
+
+name.forEach(function(name) {
+    console.log(name.toUpperCase())
+})
