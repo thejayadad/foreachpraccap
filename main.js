c6242a71708f1632fcc96af0f6a64edd2a306a10
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-07 20:52:27 -0400

    update

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
-let name = ["jon", "bob", "tom", "kenneth"]
+//Two ways to capitalize characters in an array
+
+let name = ["bob", "tom", "johnson", "kenneth"]
+
 // for (let i = 0; i < name.length; i++){
 //     console.log(name[i].toUpperCase())
 // }
 
+//Second method
 
 name.forEach(function(name) {
     console.log(name.toUpperCase())
